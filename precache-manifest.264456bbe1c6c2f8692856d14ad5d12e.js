30f36351621393f9a91d88694aa170c7fa53af2c
IgorMazur78 <spanishman0605@gmail.com>
2020-09-17 16:07:27 +0300

    Updates

---
+++ precache-manifest.264456bbe1c6c2f8692856d14ad5d12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b13bd496d367b43f0aca26a4f1c28f",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "349fc3e05c53abbdc0ef",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.96dce489.chunk.css"
+  },
+  {
+    "revision": "d31d1d628dca98ba18fe",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.73bf57fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.73bf57fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349fc3e05c53abbdc0ef",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a04596c2.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
